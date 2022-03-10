e6e8ce5eb5745ed44f68eeee42bf4a8e265986ba
shizuya-aishima <shizuya-aishima@kccs.co.jp>
2022-03-10 13:50:12 +0000

    :shirt: style: lint

--- grpc-sample/src/App.tsx
+++ grpc-sample/src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Chats } from './features/chats';
+import React from 'react'
+import logo from './logo.svg'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Chats } from './features/chats'
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
 
         <Routes>
           <Route index element={<div></div>} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/chats" element={<Chats />} />
+          <Route path='/counter' element={<Counter />} />
+          <Route path='/chats' element={<Chats />} />
         </Routes>
 
         <p>
@@ -23,44 +23,40 @@ const App = () => {
         <span>
           <span>Learn </span>
           <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://reactjs.org/'
+            target='_blank'
+            rel='noopener noreferrer'>
             React
           </a>
           <span>, </span>
           <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://redux.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'>
             Redux
           </a>
           <span>, </span>
           <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://redux-toolkit.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'>
             Redux Toolkit
           </a>
           ,<span> and </span>
           <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            className='App-link'
+            href='https://react-redux.js.org/'
+            target='_blank'
+            rel='noopener noreferrer'>
             React Redux
           </a>
         </span>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
